57105c78e295a170d1e04b5c8c129cb50d983e5d
Ed Baker <edwbaker@gmail.com>
2018-03-16 12:27:43 +0000

    Refactor for function names.

--- beatspectra/beatspectra.php
+++ beatspectra/beatspectra.php
@@ -1,48 +1,48 @@
 <?php
 
-function beat-spectra_info() {
+function beatspectra_info() {
   return(
     array(
-      "beat-spectra" => array(
+      "beatspectra" => array(
         "dependencies" => array("bioacoustica") //BioAcoustica provides wave file.
       )
     )
   );
 }
 
-function beat-spectra_init() {
+function beatspectra_init() {
   $init = array(
     "R" => array(
       "type" => "cmd",
       "required" => "required",
-      "missing text" => "beat-spectra requires R.",
+      "missing text" => "beatspectra requires R.",
       "version flag" => "--version"
     ),
     "data.table" => array( 
       "type" => "Rpackage",
       "required" => "required",
-      "missing text" => "beat-spectra requires the R data.table package.",
+      "missing text" => "beatspectra requires the R data.table package.",
       "version flag" => "--quiet -e 'packageVersion(\"data.table\")'",
       "version line" => 1
     ),
     "WaveletComp" => array( 
       "type" => "Rpackage",
       "required" => "required",
-      "missing text" => "beat-spectra requires the R WaveletComp package.",
+      "missing text" => "beatspectra requires the R WaveletComp package.",
       "version flag" => "--quiet -e 'packageVersion(\"WaveletComp\")'",
       "version line" => 1
     ),
     "phonTools" => array( 
       "type" => "Rpackage",
       "required" => "required",
-      "missing text" => "beat-spectra requires the R phonTools package.",
+      "missing text" => "beatspectra requires the R phonTools package.",
       "version flag" => "--quiet -e 'packageVersion(\"phonTools\")'",
       "version line" => 1
     ),
     "seewave" => array( 
       "type" => "Rpackage",
       "required" => "required",
-      "missing text" => "beat-spectra requires the R seewave package.",
+      "missing text" => "beatspectra requires the R seewave package.",
       "version flag" => "--quiet -e 'packageVersion(\"seewave\")'",
       "version line" => 1
     )
@@ -50,31 +50,31 @@ function beat-spectra_init() {
   return ($init);
 }
 
-function beat-spectra_prepare() {
+function beatspectra_prepare() {
   global $system;
-  core_log("info", "beat-spectra", "Attempting to list beat-spectragram files on analysis server.");
-  exec("s3cmd ls s3://bioacoustica-analysis/beat-spectra/".$system["modules"]["beat-spectra"]["git_hash"]."/", $output, $return_value);
+  core_log("info", "beatspectra", "Attempting to list beatspectragram files on analysis server.");
+  exec("s3cmd ls s3://bioacoustica-analysis/beatspectra/".$system["modules"]["beatspectra"]["git_hash"]."/", $output, $return_value);
   if ($return_value == 0) {
     if (count($output) == 0) {
-      $system["analyses"]["beat-spectra"] = array();
+      $system["analyses"]["beatspectra"] = array();
     } else {
       foreach ($output as $line) {
         $start = strrpos($line, "/");
-        $system["analyses"]["beat-spectra"][] = substr($line, $start + 1);
+        $system["analyses"]["beatspectra"][] = substr($line, $start + 1);
       }
     }
-  core_log("info", "beat-spectra", count($system["analyses"]["beat-spectra"])." beat-spectra files found.");
+  core_log("info", "beatspectra", count($system["analyses"]["beatspectra"])." beatspectra files found.");
   }
   return(array());
 }
 
-function beat-spectra_analyse($recording) {
+function beatspectra_analyse($recording) {
   global $system;
   $return = array();
-  if (!in_array($recording["id"].".png", $system["analyses"]["beat-spectra"])) {
+  if (!in_array($recording["id"].".png", $system["analyses"]["beatspectra"])) {
     $file = core_download("wav/".$recording["id"].".1kHz-highpass.wav");
     if ($file == NULL) {
-      core_log("warning", "beat-spectra", "File was not available, skipping analysis.");
+      core_log("warning", "beatspectra", "File was not available, skipping analysis.");
       return($return);
     }
     $return[$recording["id"].".1kHz-highpass.wav"] = array(
@@ -82,26 +82,26 @@ function beat-spectra_analyse($recording) {
       "local path" => "scratch/wav/",
       "save path" => NULL
     );
-    core_log("info", "beat-spectra", "Attepting to create beat-spectragram for recording ".$recording["id"].".");
-    exec("Rscript modules/traits-beat-spectra/beat-spectra/beat-spectra.R ".$recording["id"]." scratch/wav/".$recording["id"].".wav", $output, $return_value);
+    core_log("info", "beatspectra", "Attepting to create beatspectragram for recording ".$recording["id"].".");
+    exec("Rscript modules/traits-beatspectra/beatspectra/beatspectra.R ".$recording["id"]." scratch/wav/".$recording["id"].".wav", $output, $return_value);
     if ($return_value == 0) {
       $return[$recording["id"].".png"] = array(
         "file name" => $recording["id"].".png",
-        "local path" => "modules/traits-beat-spectra/beat-spectra/",
-        "save path" => "beat-spectra/".$system["modules"]["beat-spectra"]["git_hash"]."/"
+        "local path" => "modules/traits-beatspectra/beatspectra/",
+        "save path" => "beatspectra/".$system["modules"]["beatspectra"]["git_hash"]."/"
       );
       $return[$recording["id"].".csv"] = array(
         "file name" => $recording["id"].".csv",
-        "local path" => "modules/traits-beat-spectra/beat-spectra/",
-        "save path" => "beat-spectra/".$system["modules"]["beat-spectra"]["git_hash"]."/"
+        "local path" => "modules/traits-beatspectra/beatspectra/",
+        "save path" => "beatspectra/".$system["modules"]["beatspectra"]["git_hash"]."/"
       );
       $return[$recording["id"].".largest-peak-data.csv"] = array(
         "file name" => $recording["id"].".largest-peak-data.csv",
-        "local path" => "modules/traits-beat-spectra/beat-spectra/",
-        "save path" => "beat-spectra/".$system["modules"]["beat-spectra"]["git_hash"]."/"
+        "local path" => "modules/traits-beatspectra/beatspectra/",
+        "save path" => "beatspectra/".$system["modules"]["beatspectra"]["git_hash"]."/"
       );
     } else {
-      core_log("warning", "beat-spectra", "Recording ".$recording["id"].": Failed to read wave file: ".serialize($output));
+      core_log("warning", "beatspectra", "Recording ".$recording["id"].": Failed to read wave file: ".serialize($output));
       }
     }
   }
